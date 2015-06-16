335502d170fe22f2b4f92e215fc7963d1d87abe7
Nick Erlikh <n.erlikh@gmail.com>
2015-06-16 17:10:24 +0300

    combine canvas and view in drawings

--- public/lib/constants.js
+++ public/lib/constants.js
@@ -2,8 +2,3 @@ export const events = {
   DRAWING_CREATED: 'DRAWING_CREATED',
   DRAWING_FETCHED: 'DRAWING_FETCHED'
 };
-
-export const canvasSize = {
-  w: 700,
-  h: 700
-};
--- public/lib/drawings/view.js
+++ public/lib/drawings/view.js
@@ -1,5 +1,52 @@
-import canvas from './canvas'
 import actions from './actions'
-import {canvasSize} from '../constants'
 
-export default store => canvas(canvasSize.w, canvasSize.h, actions, store)
+class DrawingsCanvas {
+  constructor(store){
+    var canvas = document.getElementById('drawingsCanvas');
+    this.context = canvas.getContext('2d');
+    this.store = store;
+
+    canvas.addEventListener('mousedown', actions.mouseDown);
+    canvas.addEventListener('mousemove', actions.mouseMove);
+    canvas.addEventListener('mouseup', actions.mouseUp);
+    canvas.addEventListener('mouseleave', actions.mouseLeave);
+
+    store.registerObserver(this.storeUpdated.bind(this));
+  }
+
+  storeUpdated(){
+    this.redraw(this.store.xs, this.store.ys, this.store.draggs);
+  }
+
+  clear(){
+    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+  }
+
+  setDefaultDrawingStyle(){
+    this.context.strokeStyle = "#df4b26";
+    this.context.lineJoin = "round";
+    this.context.lineWidth = 5;
+  }
+
+  redraw(xs, ys, drags){
+    var context = this.context;
+    this.clear();
+    this.setDefaultDrawingStyle();
+
+    for (let i = 0; i < xs.length; i++) {
+      context.beginPath();
+      if (drags[i] && i) {
+        context.moveTo(xs[i - 1], ys[i - 1]);
+      } else {
+        context.moveTo(xs[i]-1, ys[i]);
+      }
+      context.lineTo(xs[i], ys[i]);
+      context.closePath();
+      context.stroke();
+    }
+  }
+}
+
+export default function(store){
+  return new DrawingsCanvas(store);
+}
