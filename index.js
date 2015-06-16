e6f1addb20ed0364337c361c1c4ea7dce7fd9e9c
Nick Erlikh <n.erlikh@gmail.com>
2015-06-16 08:20:15 +0300

    use directory path from process env

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+process.env.PWD = process.cwd();
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-app.use('/static', express.static('public'));
+app.use('/static', express.static(process.env.PWD + '/public'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
